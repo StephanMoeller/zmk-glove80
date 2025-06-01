79a1b2a4287b26a156cd5e301b274f04caf2ed2d
Stephan Moeller <stephanryer@hotmail.com>
2025-06-01 20:33:38 +0200

    moved ! to top mid

---
+++ config/test.zig
@@ -0,0 +1,19 @@
+var layer1_taps = \
+          q                               '
+    w <j> r     p     b       k     l <æ> o <å> u
+f   a <q> s <v> t     g       m     n     e <-> i   y
+    x <z> c     d                   h     ,     .  
+                     ent     spc
+/
+var extraCombos = ["lu", "ø"];
+
+defaults.tapping_term = 35;
+
+var hold_config = \ 
+           -                               -
+    -      lg     -     -       -     -     rg    -
+ -  la(50) lc(15) ls    -       -     rs    rc    ra    -
+    -      -      -                   -     -     -
+                       _1       _2
+\
+
