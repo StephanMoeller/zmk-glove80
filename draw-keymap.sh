9e65e24e6773b2f1c39fde2f153c24a499e3b568
Stephan Moeller <stephanryer@hotmail.com>
2025-06-01 22:59:14 +0200

    added select-layers

--- draw-keymap.sh
+++ draw-keymap.sh
@@ -1,4 +1,4 @@
 keymap -c keymap-drawer.config parse -z config/glove80.keymap > keymap.yaml
-keymap -c keymap-drawer.config draw keymap.yaml > keymap.svg 
+keymap -c keymap-drawer.config draw -s arrows -- keymap.yaml > keymap.svg 
 
 
