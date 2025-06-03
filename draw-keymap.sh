0597033dd9c501c44c7072ad98d3d753f39f4d70
Stephan Moeller <stephanryer@hotmail.com>
2025-06-03 08:48:34 +0200

    latest drawing

--- draw-keymap.sh
+++ draw-keymap.sh
@@ -1,5 +1,5 @@
 rm keymap.svg
 keymap -c keymap-drawer.config parse -z config/glove80.keymap > keymap.yaml
-keymap -c keymap-drawer.config draw  keymap.yaml > keymap.svg 
+keymap -c keymap-drawer.config draw -s 0  -- keymap.yaml > keymap.svg 
 
 
